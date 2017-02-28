5a73f82ac010fcf122dec7c397d47be2a0d6297a
Jasand <jas.sandmaster@gmail.com>
2017-02-27 18:28:39 -0800

    adding prototype templates for front end

--- comic_store/apps/comics/urls.py
+++ comic_store/apps/comics/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^products_main$', views.products_main),
+    url(r'^product_spotlight$', views.product_spotlight),
+    url(r'^shopping_cart$', views.shopping_cart),
+
 ]
